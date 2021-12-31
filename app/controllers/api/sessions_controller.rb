ed53b278a781274ab99995f86e13ebb564edf937
mcountryman <countryman.marco@gmail.com>
2021-12-31 18:59:25 -0500

    completed sessions/users controllers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,26 @@
 class Api::SessionsController < ApplicationController
+
+    def create 
+        @user = User.find_by_credentials(
+            params[:user][:email],
+            params[:user][:password]
+        )
+
+        if @user
+            login(@user)
+            render "api/users/show"
+        else
+            render json: ["Wrong info, dawg."], status: 401
+        end
+    end
+
+    def destroy
+        @user = current_user
+        if @user
+            logout
+            render "api/users/show"
+        else
+            render json: ["No one here."], status: 404
+        end
+    end
 end
