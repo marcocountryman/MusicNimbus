f31b66e9ec7837db6eea54aad04a552cf17f62d4
mcountryman <countryman.marco@gmail.com>
2021-12-31 12:48:12 -0500

    setup routes and generated bare controllers

---
+++ frontend/entry.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rootElement = document.getElementById('root');
+
+    ReactDOM.render(<h1>React Is Working</h1>, rootElement );
+})
