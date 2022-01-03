54606c481ae80448b43561ad74b0f5cc03f60853
mcountryman <countryman.marco@gmail.com>
2022-01-03 01:13:23 -0500

    still working on splash body

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SplashHeader from './splash_header';
+import SplashBody from './splash_body';
 
 class Splash extends React.Component {
 
     render() {
         return (
-            <div className = "splash">
+            <div className = "splash-content">
               <SplashHeader/>
+              <SplashBody/>
             </div>
         )
     }
---
+++ frontend/components/splash/splash_body.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SplashBody = () => {
+    return (
+        <div className = "splash-body" >
+            <div className = "splash-box-1">
+                <button className = "upload-btn">Upload your own</button>
+                <h2 className = "splash-message-1">Hear what's trending for free in the Music Nimbus community</h2>
+            </div>
+        </div>
+    )
+}
+
+export default SplashBody;
