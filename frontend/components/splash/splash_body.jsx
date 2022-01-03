ed2928dbef1b6e9dfc5c04b67a55a2f97ce0f7f4
mcountryman <countryman.marco@gmail.com>
2022-01-03 11:55:57 -0500

    debug before heroku push

--- frontend/components/splash/splash_body.jsx
+++ frontend/components/splash/splash_body.jsx
@@ -7,6 +7,46 @@ const SplashBody = () => {
                 <button className = "upload-btn">Upload your own</button>
                 <h2 className = "splash-message-1">Hear what's trending for free in the Music Nimbus community</h2>
             </div>
+            <div className = "splash-box-2">
+                    {/* <ul className = "song-lists">
+                        <li>
+                            <label >Song 1</label>
+                        </li>
+                        <li>
+                            <label >Song 2</label>
+                        </li>
+                        <li>
+                            <label >Song 3</label>
+                        </li>
+                        <li>
+                            <label >Song 4</label>
+                        </li>
+                        <li>
+                            <label >Song 5</label>
+                        </li>
+                        <li>
+                            <label >Song 6</label>
+                        </li>
+                        <li>
+                            <label >Song 7</label>
+                        </li>
+                        <li>
+                            <label >Song 8</label>
+                        </li>
+                        <li>
+                            <label >Song 9</label>
+                        </li>
+                        <li>
+                            <label >Song 10</label>
+                        </li>
+                        <li>
+                            <label >Song 11</label>
+                        </li>
+                        <li>
+                            <label >Song 12</label>
+                        </li>
+                    </ul> */}
+            </div>
         </div>
     )
 }
