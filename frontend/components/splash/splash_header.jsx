67a8a5be5801872a6a3179f5ca230057677f324f
mcountryman <countryman.marco@gmail.com>
2022-01-02 22:22:12 -0500

    style splash page

--- frontend/components/splash/splash_header.jsx
+++ frontend/components/splash/splash_header.jsx
@@ -6,11 +6,12 @@ const SplashHeader = () => {
     return (
         <div className = "header-content">
             <div className = "header-logo">
+                <span>☁️</span>
                 <h2>Music Nimbus</h2>
             </div>
             <div className = "header-buttons">
-                <Link to = "/signup">Create Account</Link>
-                <Link to = "/login">Sign In</Link>
+                <Link to = "/login" ><button  className = "login-btn">Sign In</button></Link>
+                <Link to = "/signup" ><button className = "signup-btn">Create Account</button></Link>
             </div>
         </div>
     )
