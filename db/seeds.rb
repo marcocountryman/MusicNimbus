55362dbea1b0677840d5d94a8ead9491696bbf20
mcountryman <countryman.marco@gmail.com>
2022-01-08 02:27:24 -0500

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,50 +1,5 @@
 require 'open-uri'
 
-#SONG CREDITS PUT THIS ON THE FRONT END
-
-# Silent Wood by Purrple Cat | https://purrplecat.com/
-# Music promoted by https://www.chosic.com/free-music/all/
-# Creative Commons CC BY-SA 3.0
-# https://creativecommons.org/licenses/by-sa/3.0/
-
-# Floating Castle by Purrple Cat | https://purrplecat.com/
-# Music promoted on https://www.chosic.com/free-music/all/
-# Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
-# https://creativecommons.org/licenses/by-sa/3.0/
-
-# Journey’s End by Purrple Cat | https://purrplecat.com/
-# Music promoted by https://www.chosic.com/free-music/all/
-# Creative Commons CC BY-SA 3.0
-# https://creativecommons.org/licenses/by-sa/3.0/
-
-# herbal tea by Artificial.Music | https://soundcloud.com/artificial-music/
-# Licensed under Creative Commons: Attribution 3.0 Unported (CC BY 3.0)
-# https://creativecommons.org/licenses/by/3.0/
-# Music promoted by https://www.chosic.com/free-music/all/ 
-
-#Study https://www.chosic.com/free-music/jazz/?mixtag=lofi
-
-# Music: https://www.chosic.com/free-music/all/ 
-# Music: https://www.chosic.com/free-music/all/ 
-# Music: https://www.chosic.com/free-music/all/ 
-
-# Music by lvymusic from Pixabay
-
-# Music by <a href="/users/lvymusic-24939435/?tab=audio&amp;utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=audio&amp;utm_content=12648">lvymusic</a> 
-# from <a href="https://pixabay.com/music/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=12648">Pixabay</a>
-
-# JuliusH, Caffeine Creek Band, Nesrality
-# https://pixabay.com/music/search/genre/smooth%20jazz/
-
-#stare8sound, Skilsel, Phantastic Beats, ETRNLPRODUCER, NbeatMusic
-# https://pixabay.com/music/search/genre/old%20school%20hip%20hop/
-
-# Music by AleXZavesa from Pixabay
-# https://pixabay.com/music/search/genre/trap/
-
-#Lesfm
-# https://pixabay.com/music/search/lofi/
-
 Song.delete_all
 User.delete_all
 ActiveRecord::Base.connection.reset_pk_sequence!('users')
@@ -60,9 +15,6 @@ user7 = User.create!(email: "mrsworldwide@hotmail.com", password: "hunter12", ag
 user8 = User.create!(email: "sayitaintso@hotmail.com", password: "hunter12", age: 24, displayname: "Ted Cruz")
 user9 = User.create!(email: "helloworld@hotmail.com", password: "hunter12", age: 26, displayname: "Marco Rubio")
 user10 = User.create!(email: "superprops@hotmail.com", password: "hunter12", age: 27, displayname: "Mitt Romney")
-
-#TITLE, ARTIST, GENRE, UPLOADER-ID
-#['Trap', 'Study', 'Jazz', 'Classical', 'Hip Hop'] 
 
 song1 = Song.create!(title: "After Midnight", artist: "Julius H", genre: 'Jazz', uploader_id: user1.id)
 image1 = URI.open('https://music-nimbus-seeds.s3.amazonaws.com/jazz-art.jpg')
@@ -213,3 +165,49 @@ image25 = URI.open('https://music-nimbus-seeds.s3.amazonaws.com/trap-art.jpg')
 song25.image_file.attach(io: image25, filename: 'trap-art.jpg')
 sound25 = URI.open('https://music-nimbus-seeds.s3.amazonaws.com/knockout-blow.mp3')
 song25.audio_file.attach(io: sound25, filename: 'knockout-blow.mp3')
+
+
+#SONG CREDITS PUT THIS ON THE FRONT END
+
+# Silent Wood by Purrple Cat | https://purrplecat.com/
+# Music promoted by https://www.chosic.com/free-music/all/
+# Creative Commons CC BY-SA 3.0
+# https://creativecommons.org/licenses/by-sa/3.0/
+
+# Floating Castle by Purrple Cat | https://purrplecat.com/
+# Music promoted on https://www.chosic.com/free-music/all/
+# Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+# https://creativecommons.org/licenses/by-sa/3.0/
+
+# Journey’s End by Purrple Cat | https://purrplecat.com/
+# Music promoted by https://www.chosic.com/free-music/all/
+# Creative Commons CC BY-SA 3.0
+# https://creativecommons.org/licenses/by-sa/3.0/
+
+# herbal tea by Artificial.Music | https://soundcloud.com/artificial-music/
+# Licensed under Creative Commons: Attribution 3.0 Unported (CC BY 3.0)
+# https://creativecommons.org/licenses/by/3.0/
+# Music promoted by https://www.chosic.com/free-music/all/ 
+
+#Study https://www.chosic.com/free-music/jazz/?mixtag=lofi
+
+# Music: https://www.chosic.com/free-music/all/ 
+# Music: https://www.chosic.com/free-music/all/ 
+# Music: https://www.chosic.com/free-music/all/ 
+
+# Music by lvymusic from Pixabay
+
+# Music by <a href="/users/lvymusic-24939435/?tab=audio&amp;utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=audio&amp;utm_content=12648">lvymusic</a> 
+# from <a href="https://pixabay.com/music/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=12648">Pixabay</a>
+
+# JuliusH, Caffeine Creek Band, Nesrality
+# https://pixabay.com/music/search/genre/smooth%20jazz/
+
+#stare8sound, Skilsel, Phantastic Beats, ETRNLPRODUCER, NbeatMusic
+# https://pixabay.com/music/search/genre/old%20school%20hip%20hop/
+
+# Music by AleXZavesa from Pixabay
+# https://pixabay.com/music/search/genre/trap/
+
+#Lesfm
+# https://pixabay.com/music/search/lofi/
