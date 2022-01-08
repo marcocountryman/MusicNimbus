7c427e531075c25ed1e2246895f4b886cd366e99
mcountryman <countryman.marco@gmail.com>
2022-01-08 15:10:57 -0500

    styling discover

--- frontend/components/discover/discover_body.jsx
+++ frontend/components/discover/discover_body.jsx
@@ -14,10 +14,10 @@ class DiscoverBody extends React.Component {
         const trapItems = this.props.songs.filter(song => song.genre === "Trap");
         const renderTrap = trapItems.map(song => {
             return (
-                <li>
-                    <img src={song.imageUrl} alt="" />
-                    <span>{song.title}</span>
-                    <span>{song.artist}</span>
+                <li className = "song-container">
+                    <img src={song.imageUrl} alt="trapsong" className= "discover-images"/>
+                    <span className = "song-title">{song.title}</span>
+                    <span className = "artist-name">{song.artist}</span>
                 </li>
             )
         })
@@ -25,10 +25,10 @@ class DiscoverBody extends React.Component {
         const hipHopItems = this.props.songs.filter(song => song.genre === "Hip Hop");
         const renderHipHop = hipHopItems.map(song => {
             return (
-                <li>
-                    <img src={song.imageUrl} alt="" />
-                    <span>{song.title}</span>
-                    <span>{song.artist}</span>
+                <li className = "song-container">
+                    <img src={song.imageUrl} alt="hiphopsong" className= "discover-images"/>
+                    <span className = "song-title">{song.title}</span>
+                    <span className = "artist-name">{song.artist}</span>
                 </li>
             )
         })
@@ -36,10 +36,10 @@ class DiscoverBody extends React.Component {
         const studyItems = this.props.songs.filter(song => song.genre === "Study");
         const renderStudy = studyItems.map(song => {
             return (
-                <li>
-                    <img src={song.imageUrl} alt="" />
-                    <span>{song.title}</span>
-                    <span>{song.artist}</span>
+                <li className = "song-container">
+                    <img src={song.imageUrl} alt="studysong" className= "discover-images"/>
+                    <span className = "song-title">{song.title}</span>
+                    <span className = "artist-name">{song.artist}</span>
                 </li>
             )
         })
@@ -47,10 +47,10 @@ class DiscoverBody extends React.Component {
         const jazzItems = this.props.songs.filter(song => song.genre === "Jazz");
         const renderJazz = jazzItems.map(song => {
             return (
-                <li>
-                    <img src={song.imageUrl} alt="" />
-                    <span>{song.title}</span>
-                    <span>{song.artist}</span>
+                <li className = "song-container">
+                    <img src={song.imageUrl} alt="jazzsong" className= "discover-images"/>
+                    <span className = "song-title">{song.title}</span>
+                    <span className = "artist-name">{song.artist}</span>
                 </li>
             )
         })
@@ -58,10 +58,10 @@ class DiscoverBody extends React.Component {
         const classicalItems = this.props.songs.filter(song => song.genre === "Classical");
         const renderClassical = classicalItems.map(song => {
             return (
-                <li>
-                    <img src={song.imageUrl} alt="" />
-                    <span>{song.title}</span>
-                    <span>{song.artist}</span>
+                <li className = "song-container">
+                    <img src={song.imageUrl} alt="classicalsong" className= "discover-images"/>
+                    <span className = "song-title">{song.title}</span>
+                    <span className = "artist-name">{song.artist}</span>
                 </li>
             )
         })
@@ -71,21 +71,57 @@ class DiscoverBody extends React.Component {
         <div className = "discover-content-container">
             <div className = "discover-content">
                 <div className = "discover-songs">
-                    <ul className = "genre-hip-hop">
-                        {renderHipHop}
-                    </ul>
-                    <ul className = "genre-trap">
-                        {renderTrap}
-                    </ul>
-                    <ul className = "genre-study">
-                        {renderStudy}
-                    </ul>
-                    <ul className = "genre-jazz">
-                        {renderJazz}
-                    </ul>
-                    <ul className = "genre-classical">
-                        {renderClassical}
-                    </ul>
+
+                    <div className = 'genre-container'>
+                        <div className = "genre-label">
+                            <h2 className = "genre-header">Hip Hop</h2>
+                            <p className = "genre-desc">Bump your head to this</p>
+                        </div>
+                        <ul className = "genre-content">
+                            {renderHipHop}
+                        </ul>  
+                    </div>
+
+                    <div className = 'genre-container'>
+                        <div className = "genre-label">
+                            <h2 className = "genre-header">Trap</h2>
+                            <p className = "genre-desc">Get down And dirty</p>
+                        </div>
+                        <ul className = "genre-content">
+                            {renderTrap}
+                        </ul>
+
+                    </div>
+
+                    <div className = 'genre-container'>
+                        <div className = "genre-label">
+                            <h2 className = "genre-header">Study</h2>
+                            <p className = "genre-desc">Woosah, study and calm</p>
+                        </div>
+                        <ul className = "genre-content">
+                            {renderStudy}
+                        </ul>
+                    </div>
+
+                    <div className = 'genre-container'>
+                        <div className = "genre-label">
+                            <h2 className = "genre-header">Jazz</h2>
+                            <p className = "genre-desc">Swing along to this</p>
+                        </div>
+                        <ul className = "genre-content">
+                            {renderJazz}
+                        </ul>
+                    </div>
+
+                    <div className = 'genre-container'>
+                        <div className = "genre-label">
+                            <h2 className = "genre-header">Classical</h2>
+                            <p className = "genre-desc">You're gonna need a cup of tea</p>
+                        </div>
+                        <ul className = "genre-content">
+                            {renderClassical}
+                        </ul>
+                    </div>
                 </div>
                 <div className = "discover-sidebar">
                     Sidebar
