dc55d4e70997427bf8a8257b269b3c29ace6e86c
mcountryman <countryman.marco@gmail.com>
2022-01-10 09:54:27 -0500

    found bug

--- frontend/components/songs/song_show.jsx
+++ frontend/components/songs/song_show.jsx
@@ -1,4 +1,4 @@
-import React from 'React'
+import React from 'react'
 import NavBarContainer from '../navbar/nav_bar_container';
 import { Link } from 'react-router-dom';
 import { shuffle } from '../../util/misc_util'
