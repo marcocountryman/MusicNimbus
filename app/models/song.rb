9033fe7b36277bab232d681384739ac9e6ed5652
mcountryman <countryman.marco@gmail.com>
2022-01-10 10:06:54 -0500

    still working on heroku

--- app/models/song.rb
+++ app/models/song.rb
@@ -13,15 +13,15 @@ class Song < ApplicationRecord
     foreign_key: :uploader_id,
     class_name: :User
 
-    def ensure_image
-        unless self.image_file.attached?
-            errors[:image_file] << "must have image file"
-        end
-    end
+    # def ensure_image
+    #     unless self.image_file.attached?
+    #         errors[:image_file] << "must have image file"
+    #     end
+    # end
 
-    def ensure_audio
-        unless self.audio_file.attached?
-            errors[:audio_file] << "must have audio file"
-        end
-    end
+    # def ensure_audio
+    #     unless self.audio_file.attached?
+    #         errors[:audio_file] << "must have audio file"
+    #     end
+    # end
 end
