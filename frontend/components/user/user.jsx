007afde2791b3bd46d53cb2a87b5d986780531bd
mcountryman <countryman.marco@gmail.com>
2022-02-04 12:25:12 -0500

    user song list rendering

--- frontend/components/user/user.jsx
+++ frontend/components/user/user.jsx
@@ -14,7 +14,21 @@ class UserShow extends React.Component {
     render() {
 
         if (!this.props.user) return null;
-
+        const userSongs = this.props.songs.filter(song => song.uploader_id === this.props.user.id);
+        const songItems = userSongs.map((song,idx) => {
+        let num = idx + 1;
+            return (
+                <li className = "user-song-list-item">
+                    <img src= {song.imageUrl} alt="song-photo" className = "song-list-image"/>
+                    <div className = "song-list-item-info">
+                        <span className = "song-number">{num}</span>
+                        <span className = "song-list-artist">{song.artist}</span>
+                        <span>-</span>
+                        <span className = "song-list-title">{song.title}</span>
+                    </div>
+                </li>
+            )
+        })
         return (
             <div className = "user-content-container">
                 <div className = "user-top">
@@ -39,7 +53,9 @@ class UserShow extends React.Component {
                     </div>
 
                     <div className = "user-songs">
-                            user 
+                        <ul className = "user-song-list">
+                            {songItems}
+                        </ul>
                     </div>
                 </div>
             </div>
