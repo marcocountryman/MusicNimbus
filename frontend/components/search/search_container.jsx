8b13544a25e0df2c736a2ad4ee6a9cc342323d09
mcountryman <countryman.marco@gmail.com>
2022-02-15 20:50:50 -0500

    search bar skeleton

--- frontend/components/search/search_container.jsx
+++ frontend/components/search/search_container.jsx
@@ -5,6 +5,15 @@ import SearchBar from './search';
 const mSTP = (state) => {
 
     return {
-        
+
     }
 }
+
+const mDTP = (dispatch) => {
+
+    return {
+
+    }
+}
+
+export default withRouter(connect(mSTP,mDTP)(SearchBar))
